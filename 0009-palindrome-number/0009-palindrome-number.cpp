127cc1342a70aa838f1269d8c43ad3c309734f34
Shreevck <jayanshsakre0509@gmail.com>
2026-09-08 23:12:16 +0530

    Time: 0 ms (100%), Space: 8.6 MB (35.89%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int carry=x;
-        long digits ;
-        long  sol=0;
-
-        if(x<0)return false;
-       while(x!=0){
-            digits=x%10;
-            sol=sol*10+digits; 
-            x=x/10;
+        if(x<0){
+            return false;
         }
-        if(sol==carry){
+        int digits;
+        long rev=0;
+        int temp=x;
+
+        while(x!=0){
+            digits=x%10;
+            rev=rev*10+digits;
+            x=x/10;
+           
+        }
+        if(rev==temp){
             return true;
-        }else
-       { return false;}
-    
-        
+        }else{
+            return false;
+        }
     }
 };
