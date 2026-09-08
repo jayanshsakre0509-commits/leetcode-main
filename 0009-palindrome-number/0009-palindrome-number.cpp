d1f1e615c18539d621c3aaa9e0aafecf10d9cad3
Shreevck <jayanshsakre0509@gmail.com>
2026-09-08 23:12:44 +0530

    Time: 0 ms (100%), Space: 8.7 MB (35.89%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,21 +3,23 @@ public:
     bool isPalindrome(int x) {
         if(x<0){
             return false;
-        }
-        int digits;
-        long rev=0;
-        int temp=x;
 
+        }
+        long rev=0;
+        int digit;
+        int temp=x;
         while(x!=0){
-            digits=x%10;
-            rev=rev*10+digits;
+            digit=x%10;
+            rev=rev*10+digit;
             x=x/10;
-           
+
         }
         if(rev==temp){
             return true;
-        }else{
+    
+        }else {
             return false;
+
         }
     }
 };
